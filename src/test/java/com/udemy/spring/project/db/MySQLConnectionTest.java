f00b44e7c35cba54d5a157f38b903622a477ac05
wdEffort <yoman3011@gmail.com>
2021-02-23 09:17:17 +0900

    Spring Project JUnit을 이용한 MySQL 연결 테스트

---
+++ src/test/java/com/udemy/spring/project/db/MySQLConnectionTest.java
@@ -0,0 +1,32 @@
+package com.udemy.spring.project.db;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLConnectionTest {
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/udemy";
+    private static final String USER = "udemy";
+    private static final String PASSWD = "1234";
+
+    /**
+     * MySQL 연결 테스트
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testConnection() throws Exception {
+        Class.forName(DRIVER);
+
+        // try ~ resource 문법(JDK 1.7 이상에서 지원하는 문법)
+        // finally를 사용하지 않아도 자동으로 자원(Connection, PrepareStatement 등)을 반납한다.
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWD)) {
+            System.out.println(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
