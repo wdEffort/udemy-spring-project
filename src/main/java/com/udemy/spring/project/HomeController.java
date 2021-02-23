b4f3921d60288b7404763396284a42eaf0a952b8
wdEffort <yoman3011@gmail.com>
2021-02-23 23:32:11 +0900

    Spring Project Logging 프레임워크 설정

--- src/main/java/com/udemy/spring/project/HomeController.java
+++ src/main/java/com/udemy/spring/project/HomeController.java
@@ -1,5 +1,7 @@
 package com.udemy.spring.project;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
+    // SLF4J Logger 인터페이스를 사용한다.
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
     public String home() {
+        LOGGER.info("HomeController#home() called ...");
+
         return "index";
     }
 }
